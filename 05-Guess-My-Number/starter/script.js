e563ff526c51630981140ba720db3eae5c5389a4
banch0u <fikret.okis@gmail.com>
2022-05-22 19:29:00 +0400

    mm

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+//const randomNumber = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+//console.log(randomNumber);
+const randomNumber = 15;
+let score = 20;
+
+function check() {
+  let guessNumber = document.querySelector(".guess").value;
+  if (guessNumber <= 0 || guessNumber > 20) {
+    document.querySelector(".message").textContent = "Please select numbers between 1 to 20";
+  }
+  else if (guessNumber == randomNumber) {
+    document.querySelector(".message").textContent = "Correct";
+    document.querySelector("body").style.backgroundColor = "Green";
+    document.querySelector(".number").textContent = randomNumber;
+    document.querySelector(".highscore").textContent = score;
+
+
+
+  }
+  else if (guessNumber > randomNumber) {
+    document.querySelector(".message").textContent = "too high";
+    score--;
+    document.querySelector(".score").textContent = score;
+
+  }
+  else if (guessNumber < randomNumber) {
+    document.querySelector(".message").textContent = "too low";
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+}
