b5af1bcf3aeb7939a152c667ddc98eda8f20447e
banch0u <fikret.okis@gmail.com>
2022-05-23 10:47:45 +0400

    added event listener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,8 +4,36 @@
 //console.log(randomNumber);
 const randomNumber = 15;
 let score = 20;
+let highscore = 0;
 
-function check() {
+// function check() {
+//   let guessNumber = document.querySelector(".guess").value;
+//   if (guessNumber <= 0 || guessNumber > 20) {
+//     document.querySelector(".message").textContent = "Please select numbers between 1 to 20";
+//   }
+//   else if (guessNumber == randomNumber) {
+//     document.querySelector(".message").textContent = "Correct";
+//     document.querySelector("body").style.backgroundColor = "Green";
+//     document.querySelector(".number").textContent = randomNumber;
+//     document.querySelector(".highscore").textContent = score;
+
+
+
+//   }
+//   else if (guessNumber > randomNumber) {
+//     document.querySelector(".message").textContent = "too high";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+
+//   }
+//   else if (guessNumber < randomNumber) {
+//     document.querySelector(".message").textContent = "too low";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+//   }
+// }
+
+document.querySelector('.check').addEventListener('click', function () {
   let guessNumber = document.querySelector(".guess").value;
   if (guessNumber <= 0 || guessNumber > 20) {
     document.querySelector(".message").textContent = "Please select numbers between 1 to 20";
@@ -16,6 +44,10 @@ function check() {
     document.querySelector(".number").textContent = randomNumber;
     document.querySelector(".highscore").textContent = score;
 
+    if (score > highscore) {
+      highscore == score;
+      document.querySelector('highscore').textContent = highscore;
+    }
 
 
   }
@@ -30,4 +62,5 @@ function check() {
     score--;
     document.querySelector(".score").textContent = score;
   }
-}
+
+});
