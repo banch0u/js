3251fbf9f7135c670252348c7dbd34e15526dedb
banch0u <fikret.okis@gmail.com>
2022-05-24 12:50:53 +0400

    added exiting model by clicking overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,12 +4,17 @@
 
 const btnOpenModal = document.querySelectorAll('.show-modal');
 const hidden = document.querySelectorAll('.hidden');
+const overlay = document.querySelector('.overlay');
 
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', function () {
     for (let j = 0; j < hidden.length; j++) {
       hidden[j].style.display = "block";
     }
-
   });
 }
+overlay.addEventListener('click', function () {
+  for (let j = 0; j < hidden.length; j++) {
+    hidden[j].style.display = "none";
+  }
+})
