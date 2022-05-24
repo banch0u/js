a428ff37b55a3f1b34fd17a88411a81cd48e34af
banch0u <fikret.okis@gmail.com>
2022-05-24 12:16:19 +0400

    somewhat working modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const hidden = document.querySelectorAll('.hidden');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    for (let j = 0; j < hidden.length; j++) {
+      hidden[j].style.display = "block";
+    }
+
+  });
+}
